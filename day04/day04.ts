42ef16709313af9a0929e5d9b30ec6d518677461
Amelia Lim <limchornguan418@gmail.com>
2024-12-21 12:37:16 +0100

    day04

--- day04/day04.ts
+++ day04/day04.ts
@@ -1,11 +1,12 @@
 // roll eyes
-
+// The perfect example of over engineering
 import { map, reverse, transpose, compose, splitAt } from "ramda";
 
 const file = Bun.file("input.txt");
 const text = await file.text();
 
 const matrix: string[][] = text.split("\n").map((line) => line.split(""));
+
 const flipHorizontal = map(reverse);
 const diagonalLeft = (matrix: string[][]): string[][] =>
 	matrix.map((row, i) => row.map((_, j) => matrix[(i + j) % row.length][j]));
@@ -36,3 +37,32 @@ const p1 = transforms
 	.flat().length;
 
 console.log("p1", p1);
+
+// ---------------------------------------------------
+
+// Part 2
+// Solution from Part 1 is not useful for part 2, but I still wanna
+// keep my over-engineering part 1 solution
+
+const hasMas = (input: string[][], y: number, x: number, dir: "lr" | "rl") => {
+	let str = "";
+	if (dir === "lr") {
+		str = input[y][x].concat(input[y + 1][x + 1], input[y + 2][x + 2]);
+	} else {
+		str = input[y][x].concat(input[y + 1][x - 1], input[y + 2][x - 2]);
+	}
+
+	return str === "MAS" || str === "SAM";
+};
+
+let xMasCount = 0;
+
+for (let y = 0; y < matrix.length - 2; y++) {
+	for (let x = 0; x < matrix.length - 2; x++) {
+		if (hasMas(matrix, y, x, "lr") && hasMas(matrix, y, x + 2, "rl")) {
+			xMasCount++;
+		}
+	}
+}
+
+console.log("p2", xMasCount);
